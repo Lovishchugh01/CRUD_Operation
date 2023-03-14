9b6b4fa7b52c805a6d8ae2b794e904f58dd8666e
Lovishchugh01 <lovishchugh01@gmail.com>
2023-03-14 14:31:54 +0530

    updates

--- backend/db.js
+++ backend/db.js
@@ -1,11 +1,15 @@
 const mongoose = require('mongoose');
 
-const mongoURI = "mongodb://localhost:27017"
+mongoose.set('strictQuery', false)
+const mongoURI = "mongodb://127.0.0.1:27017/CRUD_operation";
 
 const connectToMongo = ()=>{
-    mongoose.connect(mongoURI, ()=>{
-        console.log("Connected to Mongo Successfully");
-    })
+    mongoose.connect(mongoURI,{
+        useNewUrlParser: true,
+        useUnifiedTopology: false
+    }).then(()=>{
+            console.log("Connected to Mongo Successfully");
+    }).catch(e=>console.log(e))
 }
 
 module.exports = connectToMongo;
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -6,7 +6,7 @@ const bcrypt = require('bcryptjs');
 var jwt = require('jsonwebtoken');
 var fetchuser = require('../middleware/fetchuser');
 
-const JWT_SECRET = 'Harryisagoodb$oy';
+const JWT_SECRET = 'Lovishisagoodb$oy';
 
 // ROUTE 1: Create a User using: POST "/api/auth/createuser". No login required
 router.post('/createuser', [
@@ -102,7 +102,7 @@ router.post('/login', [
 router.post('/getuser', fetchuser,  async (req, res) => {
 
   try {
-    userId = req.user.id;
+    const userId = req.user.id;
     const user = await User.findById(userId).select("-password")
     res.send(user)
   } catch (error) {
